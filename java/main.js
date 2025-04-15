2c329de6e3c3e21a858b22377f05bd9eec934c6e
Milaim Hajni (Zeka) <amararifi1234@gmail.com>
2025-04-15 18:19:28 +0200

    e
    e

--- java/main.js
+++ java/main.js
@@ -1,7 +1,28 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
-
-menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
+const mobileMenu = document.getElementById('mobile-menu');
+const navbarMenu = document.querySelector('.navbar__menu');
+mobileMenu.addEventListener('click', () => {
+    navbarMenu.classList.toggle('active');
 });
+
+const links = document.querySelectorAll('.navbar__links, .footer__link--items a');
+links.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 80,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+const getStartedBtn = document.querySelector('.main__btn a');
+if (getStartedBtn) {
+    getStartedBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        alert('Getting Started...');
+    });
+}
