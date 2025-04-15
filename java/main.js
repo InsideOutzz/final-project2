dee68c69710623057c15eeb1a508d12e854d75db
Milaim Hajni (Zeka) <amararifi1234@gmail.com>
2025-04-15 18:51:48 +0200

    e
    e

--- java/main.js
+++ java/main.js
@@ -4,21 +4,6 @@ mobileMenu.addEventListener('click', () => {
     navbarMenu.classList.toggle('active');
 });
 
-const links = document.querySelectorAll('.navbar__links, .footer__link--items a');
-links.forEach(link => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault();
-        const targetId = link.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-        if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop - 80,
-                behavior: 'smooth'
-            });
-        }
-    });
-});
-
 const getStartedBtn = document.querySelector('.main__btn a');
 if (getStartedBtn) {
     getStartedBtn.addEventListener('click', (event) => {
